6d85a3d6f0437fd79dcf247c9ba5fa088f3d0fcc
YanaAnMar <yanamarinova10@gmail.com>
2026-05-24 00:07:49 +0300

    created Ingredient and Recipe models

--- cravely_project/cravely_app/models.py
+++ cravely_project/cravely_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    cooking_time = models.IntegerField()
+    likes = models.IntegerField(default=0)
+    ingredients = models.ManyToManyField(Ingredient)
+    favorited_by = models.ManyToManyField(User, blank=True, related_name="favorite_recipes")
+
+    def __str__(self):
+        return self.title
