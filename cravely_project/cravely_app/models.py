18abdd0a7f3f41ae7708d8ab462fe7fb42bf715a
YanaAnMar <yanamarinova10@gmail.com>
2026-05-24 00:34:39 +0300

    added class Tag to Recipe model

--- cravely_project/cravely_app/models.py
+++ cravely_project/cravely_app/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
 
@@ -14,6 +22,7 @@ class Recipe(models.Model):
     cooking_time = models.IntegerField()
     likes = models.IntegerField(default=0)
     ingredients = models.ManyToManyField(Ingredient)
+    tags = models.ManyToManyField(Tag, blank=True)
     favorited_by = models.ManyToManyField(User, blank=True, related_name="favorite_recipes")
 
     def __str__(self):
